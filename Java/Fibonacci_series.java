5c7c11a03d0c2b363d7e9d5e089d325bcdadfe60
Ashish Dhakane <dhakneashish110@gmail.com>
2026-04-16 21:53:06 +0530

    Create Fibonacci_series.java

---
+++ Java/Fibonacci_series.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    // Recursive function
+    static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        System.out.print("Fibonacci Series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        sc.close();
+    }
+}
